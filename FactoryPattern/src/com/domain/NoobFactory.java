6227b82a85020250e5ece4f74fb83c12464f6405
Nestor <huallpa.nestor@gmail.com>
2013-05-18 21:21:50 -0300

    cambios

--- FactoryPattern/src/com/domain/NoobFactory.java
+++ FactoryPattern/src/com/domain/NoobFactory.java
@@ -1,16 +1,12 @@
 package com.domain;
 
 public class NoobFactory {
-
 	public Product createProduct(String productId) {
-		
 		if (productId.equals("1")) {
 			return new VipProduct();
 		} else if (productId.equals("2")) {
 			return new CommonProduct();
 		}
-		
 		return null;
 	}
-
 }
